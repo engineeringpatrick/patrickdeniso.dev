fae83f56d78b37929c0a896cf7f9b39880339674
Patrick Deniso <patrickdeniso@gmail.com>
2025-12-03 01:41:42 -0500

    add redirect page + cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import tailwind from '@astrojs/tailwind';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://patrickdeniso.dev',
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
+
+  adapter: cloudflare(),
 });
